a1730fea5bdc96e477dfffa4044baeeba8ccc764
David Delassus <david.jose.delassus@gmail.com>
2011-11-11 18:33:11 +0100

    [Continuous integration] Add ci.sh and requirements.txt (Closes #1)

---
+++ ci.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "===> Creating virtual environment: .venv"
+virtualenv .venv || exit 1
+
+echo && echo && echo "===> Installing dependancies"
+.venv/bin/pip install -r requirements.txt || exit 1
+
+echo && echo && echo "===> Running test suite"
+.venv/bin/python2.7 manage.py test || exit 1
