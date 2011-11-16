d9ce0c71c0d70572f6c00c0025e1acc3cdf2ba85
David Delassus <david.jose.delassus@gmail.com>
2011-11-16 16:54:07 +0100

    Add staticfiles app ; Add jQuery and datatables plugin to static files ; Get all database entries and store them in a JSON tree

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join (os.getcwd (), 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join (os.getcwd (), 'templates', 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -93,6 +94,15 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
 )
 
 MIDDLEWARE_CLASSES = (
--- views.py
+++ views.py
@@ -1,13 +1,41 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import RequestContext, loader
 from trackfile.models import DatabaseEntry
 
-def home (request):
-    filelist = DatabaseEntry.objects.all ()
-    t = loader.get_template ('index.html')
+import os
 
-    c = Context ({
+def home (request):
+    entries = DatabaseEntry.objects.all ()
+
+    # Create JSON tree
+    json = {}
+
+    for dbe in entries:
+        folders = dbe.path.split (os.sep)
+        d = json
+
+        for name in folders:
+            # Is it the end of path ?
+            if name == folders[-1]:
+                d[name] = (dbe.checksum, dbe.sent)
+            # If not in the tree, create it
+            elif name not in d:
+                d[name] = {}
+
+            d = d[name]
+
+    # Create list for the table
+    filelist = []
+
+    for dbe in entries:
+        f = {'name': os.path.basename (dbe.path), 'checksum': dbe.checksum, 'size': 'unknow', 'date': dbe.sent}
+        filelist.append (f)
+
+    # Load template
+    t = loader.get_template ("index.html")
+    c = RequestContext (request, {
         'filelist': filelist,
+        'json-tree': json,
     })
 
     return HttpResponse (t.render (c))
