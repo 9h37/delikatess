66bf9f017e66c41459d1654fcb3da10b01d2536f
David Delassus <david.jose.delassus@gmail.com>
2011-12-07 10:07:27 +0100

    Restore treeview.js deleted by commit ab5fbb296a

--- static/treeview.js
+++ static/treeview.js
@@ -1,3 +1,69 @@
+function filltable (li)
+{
+    var div = li.getElementsByTagName ('div')[0];
+    var children = div.children[0].children;
+
+    $("#table").empty ();
+
+    for (var i = 0, c = children.length; i < c; i++)
+    {
+        var a = children[i].getElementsByTagName ('a')[0];
+        var cdiv = children[i].getElementsByTagName ('div')[0];
+
+        var img = $('<img/>');
+        img.attr ({
+            alt: "",
+            src: (cdiv.hasChildNodes () ? "/static/images/folder-closed.gif" : "/static/images/file.gif")
+        });
+
+        // create elements for the table
+        var tr = $('<tr/>');
+
+        // name
+        var td = $('<td/>');
+        td.append (img);
+
+        if (cdiv.hasChildNodes ())
+        {
+            var link = $('<a/>');
+
+            link.attr ({
+                href: "#",
+                title: a.title
+            });
+            link.text (a.innerHTML);
+            link.PARENT = a.parentNode;
+            link.click (function () {
+                toggle (link.PARENT, true);
+            });
+            td.append (link);
+        }
+        else
+            td.append (a.innerHTML);
+
+        tr.append (td);
+
+        // size
+        td = $('<td/>');
+        td.text ("unknow");
+        tr.append (td);
+
+        // date
+        td = $('<td/>');
+        td.text ("0");
+        tr.append (td);
+
+        // actions
+        td = $('<td/>');
+        td.text ("X");
+        td.a = a;
+        tr.append (td);
+
+        $("#table").append (tr);
+    }
+}
+
+// Integrate treeview with the topbar
 function go (name, li)
 {
     var nli = $('<li/>');
@@ -10,7 +76,8 @@ function go (name, li)
     // go to this link.
     nlink.click (function () {
         $('#nav').empty ();
-        go (name,li);
+        go (name, li);
+        filltable (li);
     });
 
     nli.append (nlink);
@@ -31,7 +98,8 @@ function go (name, li)
     nli.attr ("class", "active");
 }
 
-function toggle (el)
+// toggle visiblity of children in the treeview
+function toggle (el, alwaysShow)
 {
     var children = el.getElementsByTagName ('div')[0];
 
@@ -40,29 +108,40 @@ function toggle (el)
     if (children.hasChildNodes () == false)
         return;
 
+
     $('#nav').empty ();
 
     // if children are hidden, show them
-    if (children.style.display == "none" || children.style.display == "")
+    if (alwaysShow == true || children.style.display == "none" || children.style.display == "")
     {
         children.style.display = "block";
-        el.style.backgroundImage = "url('{{ STATIC_URL }}images/folder.gif')"
+        el.style.backgroundImage = "url('/static/images/folder.gif')"
 
         // go to the current level
         go (el.getElementsByTagName ('a')[0].innerHTML, el);
+
+        // now fill the table
+        filltable (el);
     }
     else // if they are visible, hide them
     {
         children.style.display = "none";
-        el.style.backgroundImage = "url('{{ STATIC_URL }}images/folder-closed.gif')"
+        el.style.backgroundImage = "url('/static/images/folder-closed.gif')"
 
         // go to the parent level
         var li = el.parentNode.parentNode.parentNode;
         if (li.tagName == "LI")
+        {
             go (li.getElementsByTagName ('a')[0].innerHTML, li);
+
+            // now fill the table
+            filltable (li);
+        }
     }
+
 }
 
+// Remove files from the treeview
 function render ()
 {
     var divs = $('.children');
@@ -73,7 +152,7 @@ function render ()
         if (children.hasChildNodes () == false)
         {
             li = children.parentNode;
-            li.style.backgroundImage = "url('{{ STATIC_URL }}images/file.gif')";
+            li.style.display = "none";
         }
     }
 }
